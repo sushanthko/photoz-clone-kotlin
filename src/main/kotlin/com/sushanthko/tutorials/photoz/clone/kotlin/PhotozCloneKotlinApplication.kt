fa2d788ea3132e8d6f536216973fcdb7b2b540f9
sushanthko <sushanthkotian.s@gmail.com>
2024-12-24 16:13:06 +0100

    First commit

---
+++ src/main/kotlin/com/sushanthko/tutorials/photoz/clone/kotlin/PhotozCloneKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.sushanthko.tutorials.photoz.clone.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotozCloneKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhotozCloneKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/sushanthko/tutorials/photoz/clone/kotlin/PhotozCloneKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sushanthko.tutorials.photoz.clone.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotozCloneKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
